b605835099948a775dcf2dde20075baa44320289
shinyj0 <yejins0605@gmail.com>
2026-03-25 14:40:50 +0900

    feat: router 구현

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -6,6 +6,7 @@ import LoginPage from "@/pages/auth/LoginPage";
 import SignUpPage from "@/pages/auth/SignUpPage";
 import OAuthCallbackPage from "@/pages/auth/OauthCallbackPage";
 import StockList from "@/pages/stocks/StockList";
+import StockDetailPage from "@/pages/stocks/StockDetailPage";
 import ProtectedRoute from "./ProtectedRoute";
 import TradeDiaryPage from "@/pages/trade-diaries/TradeDiaryPage";
 import DiaryDetailPage from "@/pages/trade-diaries/[tradeDiaryId]/DiaryDetailPage";
@@ -19,7 +20,13 @@ export const router = createBrowserRouter([
         element: <Layout />,
         children: [
           { index: true, element: <HomePage /> },
-          { path: "invest", element: <StockList /> },
+          {
+            path: "invest",
+            children: [
+              { index: true, element: <StockList /> },
+              { path: ":stockCode", element: <StockDetailPage /> },
+            ],
+          },
           { path: "components", element: <ComponentsPage /> },
           {
             path: "trade-diary",
