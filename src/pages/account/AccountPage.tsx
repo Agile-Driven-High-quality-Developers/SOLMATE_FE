7e2ef18e1b5047460067ed84987de4c159695da6
shinyj0 <yejins0605@gmail.com>
2026-03-25 21:54:25 +0900

    feat: 계좌 페이지 틀 만들기

---
+++ src/pages/account/AccountPage.tsx
@@ -0,0 +1,10 @@
+export default function AccountPage() {
+  return (
+    <div className="flex flex-col h-full p-6 gap-5 overflow-auto bg-gray-50 min-h-screen">
+      <div>
+        <h1 className="text-[22px] font-bold text-gray-900">내 계좌</h1>
+        <p className="text-[13px] text-gray-400 mt-0.5">나의 투자 현황을 한눈에 확인하세요</p>
+      </div>
+    </div>
+  );
+}
